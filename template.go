04c1d294b6adf2bc8d0aaf6cb7d24d4f565e7cbf
Sam Morrison <schmorrison@gmail.com>
2019-10-19 19:25:53 -0600

    embed static files

--- template.go
+++ template.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/schmorrison/simple-upload/assets"
@@ -10,35 +11,40 @@ import (
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
 
-	data, err := assets.FS.Open("basic.min.css")
-	data, err := assets.FS.Open("dropzone.min.js")
+	staticRes, err := getStaticFiles()
+	if err != nil {
+		err = fmt.Errorf("Failed to get static resources: %s", err)
+		fmt.Println(err)
+		return
+	}
 
 	templatePage := template.New("UploadFilePage")
-	t := template.Must(templatePage.Parse(`<!DOCTYPE html>
-
-	<html>
-		<head>
+	t := template.Must(templatePage.Parse(
+		fmt.Sprintf(`
+<!DOCTYPE html>
+<html>
+	<head>
 		<meta charset="utf-8">
 		<title>Simple Upload</title>
 
-		<script src="/res/dropzone.min.js"></script>
-		<link rel="stylesheet" href="/res/dropzone.min.css">
-		</head>
+		<script>%s</script>
+		<style>%s</style>
+	</head>
+	
+	<body>
+		<p>
+		This is the most minimal example of Dropzone. The upload in this example
+		doesn't work, because there is no actual server to handle the file upload.
+		</p>
 		
-		<body>
-			<p>
-			This is the most minimal example of Dropzone. The upload in this example
-			doesn't work, because there is no actual server to handle the file upload.
-			</p>
-			
-			<form action="/upload" method="post" enctype="multipart/form-data" class="dropzone">
-				<div class="fallback">
-					<input name="file" type="file" />
-				</div>
-			</form>
-		</body>
-	</html>
-	`))
+		<form action="/upload" method="post" enctype="multipart/form-data" class="dropzone">
+			<div class="fallback">
+				<input name="file" type="file" />
+			</div>
+		</form>
+	</body>
+</html>
+`, staticRes["JS"], staticRes["CSS"])))
 
 	if err := t.Execute(w, ""); err != nil {
 		err = fmt.Errorf("Failed to execute template: %s", err)
@@ -47,4 +53,38 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var css = `.dropzone,.dropzone *{box-sizing:border-box}.dropzone{position:relative}.dropzone .dz-preview{position:relative;display:inline-block;width:120px;margin:0.5em}.dropzone .dz-preview .dz-progress{display:block;height:15px;border:1px solid #aaa}.dropzone .dz-preview .dz-progress .dz-upload{display:block;height:100%;width:0;background:green}.dropzone .dz-preview .dz-error-message{color:red;display:none}.dropzone .dz-preview.dz-error .dz-error-message,.dropzone .dz-preview.dz-error .dz-error-mark{display:block}.dropzone .dz-preview.dz-success .dz-success-mark{display:block}.dropzone .dz-preview .dz-error-mark,.dropzone .dz-preview .dz-success-mark{position:absolute;display:none;left:30px;top:30px;width:54px;height:58px;left:50%;margin-left:-27px}`
+func getStaticFiles() (m map[string]string, err error) {
+	cssFile, err := assets.FS.Open("basic.min.css")
+	if err != nil {
+		err = fmt.Errorf("Failed to load CSS file: %s", err)
+		fmt.Println(err)
+		return
+	}
+
+	css, err := ioutil.ReadAll(cssFile)
+	if err != nil {
+		err = fmt.Errorf("Failed to load CSS file: %s", err)
+		fmt.Println(err)
+		return
+	}
+
+	m["css"] = string(css)
+
+	jsFile, err := assets.FS.Open("dropzone.min.js")
+	if err != nil {
+		err = fmt.Errorf("Failed to load JS file: %s", err)
+		fmt.Println(err)
+		return
+	}
+
+	js, err := ioutil.ReadAll(jsFile)
+	if err != nil {
+		err = fmt.Errorf("Failed to read JS file: %s", err)
+		fmt.Println(err)
+		return
+	}
+
+	m["js"] = string(js)
+
+	return
+}
